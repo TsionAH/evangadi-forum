3a97c915ab059e5996193faa86fdfa30d92da472
TsionAH <tsionalemuhandiso@gmail.com>
2026-01-16 15:36:58 +0300

    git add

--- client/src/components/ChatWidget/ChatWidget.jsx
+++ client/src/components/ChatWidget/ChatWidget.jsx
@@ -1,7 +1,7 @@
 import { useState, useRef, useEffect } from "react";
 import { MessageCircle, X } from "lucide-react";
 import styles from "./ChatWidget.module.css";
-import ChatBot from "./ChatBot/chatBot";
+import ChatBot from "./ChatBot/ChatBot";
 
 function ChatWidget() {
   const [isOpen, setIsOpen] = useState(false);
