84982cedc9d77ed2afe1f9c5ffc27774515b18c0
TsionAH <tsionalemuhandiso@gmail.com>
2026-01-16 15:20:46 +0300

    adding the url

--- client/src/Api/axiosConfig.js
+++ client/src/Api/axiosConfig.js
@@ -2,7 +2,8 @@ import axios from "axios";
 
 const axiosBase = axios.create({
   // baseURL: "http://localhost:5500/api",
-  baseURL: "https://evangadiforum.be.birhann.com/api",
+  //baseURL: "https://evangadiforum.be.birhann.com/api",
+  baseURL:" https://evangadi-forum-k32f.onrender.com"
 });
 
 export default axiosBase;
